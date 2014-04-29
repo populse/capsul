dbaded152cf6e6586acada581709b78f63ce24e6
Antoine Grigis <antoine.grigis@cea.fr>
2014-04-29 12:00:12 +0000

    capsul/apps_qt/base/board/board_gui_builder: find all the view node and add them in the result board.
    git-svn-id: https://bioproj.extra.cea.fr/svn/capsul/trunk@118 89a606c0-f330-4fd0-a228-5aafc7605a1d

--- capsul/apps_qt/base/board/board_gui_builder.py
+++ capsul/apps_qt/base/board/board_gui_builder.py
@@ -10,6 +10,7 @@
 # System import
 import logging
 import subprocess
+import os
 
 # Qt import
 from capsul.apps_qt.qt_backend import QtGui, QtCore
@@ -18,6 +19,10 @@ try:
 except AttributeError:
     _fromUtf8 = lambda s: s
 
+# Capsul import
+from capsul.pipeline import Pipeline
+from capsul.pipeline.pipeline_nodes import Switch, PipelineNode
+from capsul.controller import trait_ids
 
 class BoardGUIBuilder(QtGui.QWidget):
     """ Create the result board of a pipeline.
@@ -81,27 +86,73 @@ class BoardGUIBuilder(QtGui.QWidget):
 
         # Generate controller controls
         for node_name, node in self._pipeline.nodes.iteritems():
-            # add Processing
-            if node_name is not "" and node.node_type not in ["view_node"]:
+
+            # Add Processing
+            if node_name is not "" and node.node_type != "view_node":
+
+                # First borwse node to get leafs and viewers
+                process_nodes = []
+                view_nodes = []
+                self.browse_node(node, process_nodes, view_nodes,
+                                 self._pipeline)
+
+                # Enter a new item in the list
                 child = QtGui.QTreeWidgetItem(root)
                 child.setText(1, self._title_for(node_name))
+
+                # Set viewers
+                for viewer_node, pipeline in view_nodes:
+                    widget = ViewerWidget(viewer_node.name, pipeline,
+                                          self._study_config)
+                    widget.setParent(root.treeWidget())
+                    child.treeWidget().setItemWidget(child, 4, widget)
+
+            # Set the pipeline viewer directly on the root item
             elif node.node_type == "view_node":
                 widget = ViewerWidget(node_name, self._pipeline,
                                       self._study_config)
                 widget.setParent(root.treeWidget())
-                # root.setText(4, "view")
                 root.treeWidget().setItemWidget(root, 4, widget)
 
-           # child.treeWidget().setItemWidget(child, 2, widget)
-            
-            
+    def browse_node(self, node, process_nodes, view_nodes, parent_pipeline):
+        """ Find view_node and leaf nodes, ie Process nodes
+
+        Parameters
+        ----------
+        node: Node
+            a capsul node
+        process_nodes: Node
+            node of type processing_node
+        view_nodes: Node
+            node of type view_node
+        """
+        # Skip Switch nodes
+        if not isinstance(node, Switch):
+
+            # Browse recursively pipeline nodes
+            if (isinstance(node.process, Pipeline) and
+                node.node_type != "view_node"):
+
+                pipeline = node.process
+                for sub_node in pipeline.nodes.values():
+                    if not isinstance(sub_node, PipelineNode):
+                        self.browse_node(sub_node, process_nodes, view_nodes,
+                                         pipeline)
+            # Update the results according to the node type
+            else:
+                if node.node_type == "view_node":
+                    view_nodes.append((node, parent_pipeline))
+                else:
+                    process_nodes.append(node)
+
+
 class ViewerWidget(QtGui.QWidget):
     """ View result class
     """
-    
+
     def __init__(self, viewer_node_name, pipeline, study_config):
         """ Method to initialize a ViewConrol class.
-        
+
         Parameters
         ----------
         viewer_node_name: str
@@ -112,45 +163,67 @@ class ViewerWidget(QtGui.QWidget):
         """
         # Inheritance
         super(ViewerWidget, self).__init__()
-        
+
         # Default parameters
         self.viewer_node_name = viewer_node_name
         self.pipeline = pipeline
         self.study_config = study_config
-        
+
         # Build control
         button = QtGui.QToolButton(self)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/icones/view_result")),
                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
         button.setIcon(icon)
-        button.clicked.connect(self.onDelListClicked)
-        
-    def onDelListClicked(self):
+        button.clicked.connect(self.onCreateViewerClicked)
+
+    def onCreateViewerClicked(self):
         """ Event to create the viewer
         """
         # Get the viewer node and process
         viewer_node = self.pipeline.nodes[self.viewer_node_name]
         viewer_process = viewer_node.process
-        
+
         # Propagate the parameters to the input viewer node
         # And check if the viewer is active (ie dependencies
         # are specified -> corresponding process have run)
         is_viewer_active = True
         for plug_name, plug in viewer_node.plugs.iteritems():
+            
             if plug_name in ["nodes_activation", "selection_changed"]:
                 continue
-            # since it is a viewer node we normally have only inputs
-            for (source_node_name, source_plug_name, source_node, 
+            
+            # Since it is a viewer node we normally have only inputs
+            for (source_node_name, source_plug_name, source_node,
                 source_plug, weak_link) in plug.links_from:
-                
+
+                # Get the source plug value and source trait
                 source_plug_value = getattr(source_node.process,
                                             source_plug_name)
-                source_trait = source_node.process.trait(source_plug_name) 
-                setattr(viewer_process, plug_name, source_plug_value)
+                source_trait = source_node.process.trait(source_plug_name)
+
+                # Check if the viewer is active:
+                # 1) the source_plug_value has been set
                 if source_plug_value == source_trait.handler.default_value:
-                   is_viewer_active = False 
-       
+                   is_viewer_active = False
+                   break
+                # 2) if the plug is a file, the file exists
+                str_description = trait_ids(source_trait)
+                if (len(str_description) == 1 and
+                    str_description[0] == "File" and
+                    not os.path.isfile(source_plug_value)):
+
+                    is_viewer_active = False
+                    break
+                    
+                # Update destination trait
+                setattr(viewer_process, plug_name, source_plug_value)
+
+            # Just stop the iterations if the status of the viewer
+            # is alreadu known
+            if not is_viewer_active:
+                break
+
         # Execute the viewer process using the defined study configuration
         if is_viewer_active:
             subprocess.Popen(viewer_process.get_commandline())
