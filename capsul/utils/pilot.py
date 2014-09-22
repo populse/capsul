a395090a002605cdce3768afeb35b30e4cc8c6b7
Antoine Grigis <antoine.grigis@cea.fr>
2014-09-22 14:08:00 +0000

    capsul/utils/pilot: add decorator to run pilots in cleaned namespace.
    git-svn-id: https://bioproj.extra.cea.fr/svn/capsul/trunk@323 89a606c0-f330-4fd0-a228-5aafc7605a1d

---
+++ capsul/utils/pilot.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+##########################################################################
+# CAPSUL - Copyright (C) CEA, 2013
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+# System import
+import types
+
+
+def pilotfunction(pilot_func):
+    """ Function that can be used as a decorator for pilot functions.
+
+    In order to test properly the module, remove the dictionary that holds
+    the function's global variables, except the builtins functions.
+
+    Parameters
+    ----------
+    pilot_func: @func (mandatory)
+        a function that will be decorated.
+    """
+    return types.FunctionType(
+        pilot_func.func_code, {"__builtins__": __builtins__})
---
+++ capsul/utils/test/test_pilot.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+##########################################################################
+# CAPSUL - Copyright (C) CEA, 2013
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+# System import
+import numpy
+import os
+import unittest
+
+# Capsul import
+from capsul.utils.pilot import pilotfunction
+
+
+# A class
+class Dummy(object):
+    """ A dummy class.
+    """
+    def __call__(self):
+        print "Calling Dummy"
+
+
+# The decorated pilot
+@pilotfunction
+def pilot():
+    from test_pilot import Dummy
+    print ",".join(["t", "o"])
+    dummy = Dummy()
+    dummy()
+
+
+if __name__ == "__main__":
+    pilot()
