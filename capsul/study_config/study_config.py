391eaa75b70b0e8bfd2e798b345eff635830244d
Antoine Grigis <antoine.grigis@cea.fr>
2015-01-14 16:49:47 +0000

    capsul/study_config: update the run method to integrate the new improvments + test.
    git-svn-id: https://bioproj.extra.cea.fr/svn/capsul/trunk@386 89a606c0-f330-4fd0-a228-5aafc7605a1d

--- capsul/study_config/study_config.py
+++ capsul/study_config/study_config.py
@@ -20,7 +20,7 @@ else:
 logger = logging.getLogger(__name__)
 
 # Trait import
-from traits.api import Directory, File, Bool, Instance, String, Undefined
+from traits.api import Directory, Bool, String, Undefined
 
 # Soma import
 from soma.controller import Controller
@@ -28,11 +28,10 @@ from soma.controller import Controller
 # Capsul import
 from capsul.pipeline import Pipeline
 from capsul.process import Process
-from run import _run_process
+from run import run_process
 from capsul.pipeline.pipeline_workflow import (
     workflow_from_pipeline, local_workflow_run)
-from capsul.pipeline.pipeline_nodes import IterativeNode
-
+from capsul.pipeline.pipeline_nodes import IterativeNode, Node
 
 
 class StudyConfig(Controller):
@@ -43,7 +42,7 @@ class StudyConfig(Controller):
     StudyConfig has modules (see BrainVISAConfig, FSLConfig, MatlabConfig,
     SmartCachingConfig, SomaWorkflowConfig, SPMConfig, FOMConfig).
     Modules are initialized in the constructor, so their list has to be setup
-    before instantiating StudyConfig. A default modules list is used when no 
+    before instantiating StudyConfig. A default modules list is used when no
     modules are specified: StudyConfig.default_modules
 
     StudyConfig configuration is loaded from a global file and then from a
@@ -58,8 +57,8 @@ class StudyConfig(Controller):
 
       study_config = StudyConfig(modules=['SPMConfig', 'FomConfig'])
       # or:
-      study_config = StudyConfig(modules=StudyConfig.default_modules + ['FomConfig'])
-
+      study_config = StudyConfig(modules=StudyConfig.default_modules +
+      ['FomConfig'])
 
 
     Attributes
@@ -86,31 +85,33 @@ class StudyConfig(Controller):
     default_modules = ['FSLConfig', 'MatlabConfig', 'SmartCachingConfig',
                        'SomaWorkflowConfig', 'SPMConfig']
     _user_config_directory = "~/.config/capsul"
-    
+
     study_name = String(
         None,
         desc="Name of the study to configure",
         # traits with transient=True will not be saved in configuration
-        # see  http://code.enthought.com/projects/traits/docs/html/traits_user_manual/advanced.html#pickling-hastraits-objects
+        # see  http://code.enthought.com/projects/traits/docs/html/
+        # traits_user_manual/advanced.html#pickling-hastraits-objects
         transient=True)
 
     input_directory = Directory(
         Undefined,
         desc="Parameter to set the study input directory")
-    
+
     output_directory = Directory(
         Undefined,
         desc="Parameter to set the study output directory")
-    
+
     generate_logging = Bool(
         False,
         desc="Parameter to control the log generation")
-    
+
     automatic_configuration = Bool(
         False,
         desc="If True, tries to automatically setup configuration on startup")
 
-    def __init__(self, study_name=None, init_config=None, modules=None, **override_config):
+    def __init__(self, study_name=None, init_config=None, modules=None,
+                 **override_config):
         """ Initilize the StudyConfig class
 
         Parameters
@@ -121,20 +122,20 @@ class StudyConfig(Controller):
             to configure this StudyConfig (instead of reading configuration
             from configuration files).
         modules: list of string (default self.default_modules).
-            the names of configuration module classes that will be included in this 
-            study configuration.
+            the names of configuration module classes that will be included
+            in this study configuration.
         override_config: dictionary
-            The content of these keyword parameters will be set on the 
-            configuration after it has been initialized from configuration 
+            The content of these keyword parameters will be set on the
+            configuration after it has been initialized from configuration
             files (or from init_config).
         """
-        
+
         # Inheritance
         super(StudyConfig, self).__init__()
-        
+
         if study_name:
             self.study_name = study_name
-        
+
         # Read the configuration for the given study
         if init_config is None:
             config = self.read_configuration()
@@ -147,33 +148,32 @@ class StudyConfig(Controller):
                 config.update(override_config)
             else:
                 config = init_config
-        
+
         # Create modules
         if modules is None:
             # Make it possible for a study to define its own set of modules
             modules = config.pop('config_modules', self.default_modules)
-            
+
         # 'modules_data' is a container for modules-specific internal data
         # each module is encouraged to prefix its variables there by its
         # module name
         self.modules_data = Controller()
-        
+
         self.modules = {}
         for module in modules:
             self.load_module(module, config)
-        
+
         # Set self attributes according to configuration values
         for k, v in config.iteritems():
             setattr(self, k, v)
         self.initialize_modules()
-        
-        
+
     def initialize_modules(self):
         """
         Modules initialization, calls initialize_module on each config module.
         This is not done during module instanciation to allow interactions
         between modules (e.g. Matlab configuration can influence Nipype
-        configuration). Modules dependencies are taken into account in 
+        configuration). Modules dependencies are taken into account in
         initialization.
         """
         already_initialized = set()
@@ -189,7 +189,8 @@ class StudyConfig(Controller):
                                        'missing' % module_name)
             # Check if there are dependent modules that must be initilaized
             # before the current one
-            initialize_first = [m for m in module.dependencies if m not in already_initialized]
+            initialize_first = [m for m in module.dependencies
+                                if m not in already_initialized]
             if initialize_first:
                 stack = initialize_first + [module_name] + stack
                 continue
@@ -204,11 +205,6 @@ class StudyConfig(Controller):
         # Parameter that is incremented at each process execution
         self.process_counter = 1
 
-        # Set the caller
-        self._caller = _run_process
-
-
-
     ####################################################################
     # Methods
     ####################################################################
@@ -216,24 +212,33 @@ class StudyConfig(Controller):
     def load_module(self, config_module_name, config):
         """
         Load an optional StudyConfig module.
-        
+
         Parameters
         ----------
         config_module_name: Name of the module to load (e.g. "FSLConfig").
         config: dictionary containing the configuration of the study.
         """
         if config_module_name not in self.modules:
-            python_module = 'capsul.study_config.config_modules.%s_config' % \
-                        config_module_name[:-6].lower()
-            python_module = __import__(python_module, fromlist=[config_module_name])
+            python_module = (
+                "capsul.study_config.config_modules.{0}_config".format(
+                    config_module_name[:-6].lower()))
+            python_module = __import__(python_module,
+                                       fromlist=[config_module_name])
             config_module_class = getattr(python_module, config_module_name)
             module = config_module_class(self, config)
             self.modules[config_module_name] = module
             return module
-    
-    def run(self, process_or_pipeline, executer_qc_nodes=False):
-        """ Method to execute a process or a pipline in a study
-        configuration environment.
+
+    def run(self, process_or_pipeline, executer_qc_nodes=False, verbose=1,
+            **kwargs):
+        """ Method to execute a process or a pipline in a study configuration
+         environment.
+
+         Only pipeline nodes can be filtered on the 'executer_qc_nodes'
+         attribute.
+
+         A valid output directory is exepcted to execute the process or the
+         pepeline without soma-workflow.
 
         Parameters
         ----------
@@ -242,6 +247,8 @@ class StudyConfig(Controller):
         execute_qc_nodes: bool (optional, default False)
             if True execute process nodes that are taged as qualtity control
             process nodes.
+        verbose: int
+            if different from zero, print console messages.
         """
         # Use soma worflow to execute the pipeline or porcess in parallel
         # on the local machine
@@ -256,9 +263,10 @@ class StudyConfig(Controller):
             # FIXME: it would be better if study_config does not require
             # soma_workflow modules.
             from soma_workflow import constants as swconstants
-            self.failed_jobs = [element for element in elements_status[0] \
-                if element[1] != swconstants.DONE \
-                    or element[3][0] != swconstants.FINISHED_REGULARLY]
+            self.failed_jobs = [
+                element for element in elements_status[0]
+                if element[1] != swconstants.DONE
+                or element[3][0] != swconstants.FINISHED_REGULARLY]
             # if execution was OK, delete the workflow
             if workflow_status == swconstants.WORKFLOW_DONE \
                     and len(self.failed_jobs) == 0:
@@ -273,22 +281,36 @@ class StudyConfig(Controller):
         # Use the local machine to execute the pipeline or process
         else:
 
+            # Check the output directory is valid
+            if (self.output_directory is Undefined or
+                    not isinstance(self.output_directory, str)):
+                raise ValueError(
+                    "'{0}' is not a valid directory. A valid output "
+                    "directory is expected to run the process or "
+                    "pipeline.".format(self.output_directory))
+            try:
+                if not os.path.isdir(self.output_directory):
+                    os.makedirs(self.output_directory)
+            except:
+                raise ValueError(
+                    "Can't create folder '{0}', please investigate.".format(
+                        self.output_directory))
+
             # Generate ordered execution list
             execution_list = []
             if isinstance(process_or_pipeline, Pipeline):
                 execution_list = process_or_pipeline.workflow_ordered_nodes()
+                # Filter process nodes if necessary
+                if not executer_qc_nodes:
+                    execution_list = [node for node in execution_list
+                                      if node.node_type != "view_node"]
             elif isinstance(process_or_pipeline, Process):
                 execution_list.append(process_or_pipeline)
             else:
                 raise Exception(
-                    "Unknown instance type. Got {0}"
-                    "and expect Process or Pipeline"
-                    "instances".format(process_or_pipeline.__module__.name__))
-
-            # Filter process nodes if necessary
-            if not executer_qc_nodes:
-                execution_list = [node for node in execution_list
-                                  if node.node_type != "view_node"]
+                    "Unknown instance type. Got {0}and expect Process or "
+                    "Pipeline instances".format(
+                        process_or_pipeline.__module__.name__))
 
             # Execute each process node element
             for process_node in execution_list:
@@ -312,50 +334,63 @@ class StudyConfig(Controller):
 
                     # Execute the iterative process instances
                     for iterative_process_node in iterative_execution_list:
-                        self._run(iterative_process_node.process)
+                        self._run(iterative_process_node.process, verbose,
+                                  **kwargs)
+
+                # Execute the process instance contained in the node
+                elif isinstance(process_node, Node):
+                    self._run(process_node.process, verbose, **kwargs)
 
                 # Execute the process instance
                 else:
-                    self._run(process_node.process)
+                    self._run(process_node, verbose, **kwargs)
 
-    def _run(self, process_instance):
+    def _run(self, process_instance, verbose, **kwargs):
         """ Method to execute a process in a study configuration environment.
 
         Parameters
         ----------
         process_instance: Process instance (mandatory)
             the process we want to execute
+        verbose: int
+            if different from zero, print console messages.
         """
         # Message
-        logger.info("Study Config: executing process "
-                    "'{0}'...".format(process_instance.id))
+        logger.info("Study Config: executing process '{0}'...".format(
+            process_instance.id))
 
         # Run
-        returncode, log_file = self._caller(
+        destination_folder = os.path.join(
             self.output_directory,
-            "{0}-{1}".format(self.process_counter,
-                             process_instance.name),
+            "{0}-{1}".format(self.process_counter, process_instance.name))
+        if self.get_trait_value("use_smart_caching") in [None, False]:
+            cachedir = None
+        else:
+            cachedir = self.output_directory
+        returncode, log_file = run_process(
+            destination_folder,
             process_instance,
+            cachedir,
             self.generate_logging,
-            self.spm_directory)
+            **kwargs)
 
         # Increment the number of executed process count
         self.process_counter += 1
 
     def reset_process_counter(self):
-        """ Method to reset the process counter to one
+        """ Method to reset the process counter to one.
         """
         self.process_counter = 1
 
     def read_configuration(self):
-        """Find the configuration for the current study (whose name is defined 
+        """Find the configuration for the current study (whose name is defined
         in self study_name) and returns a dictionary that is a merge between
         global options and study specific options.
-        
+
         Global option are taken from environment variable CAPSUL_CONFIG if
         it is defined, otherwise from "~/.config/capsul/config.json" if it
         exists.
-        
+
         The configuration for a study can be defined the global configuration
         if it contains a dictionary in it "studies_config" option and if there
         is a key corresponding to self.study_name in this dictionary. If the
@@ -364,10 +399,10 @@ class StudyConfig(Controller):
         file). Otherwise, the corresponding value must be a dictionary
         containing study specific configuration values.
         If no study configuration is found from global configuration, then
-        a file named "~/.config/capsul/%s/config.json" (where %s is 
+        a file named "~/.config/capsul/%s/config.json" (where %s is
         self.study_name) is used if it exists.
         """
-        
+
         # First read global options
         global_config_file = os.environ.get("CAPSUL_CONFIG")
         if global_config_file:
@@ -383,7 +418,7 @@ class StudyConfig(Controller):
             else:
                 config = {}
                 self.global_config_file = None
-        
+
         # Look for study specific configuration file
         study_config = \
             config.pop('studies_config', {}).get(self.study_name)
@@ -396,8 +431,9 @@ class StudyConfig(Controller):
             study_config = json.load(open(study_config))
         elif study_config is None:
             study_config_file = \
-                os.path.expanduser(os.path.join(self._user_config_directory,
-                "%s/config.json") % str(self.study_name))
+                os.path.expanduser(
+                    os.path.join(self._user_config_directory,
+                                 "%s/config.json") % str(self.study_name))
             if os.path.exists(study_config_file):
                 study_config = json.load(open(study_config_file))
                 self.study_config_file = study_config_file
@@ -406,12 +442,12 @@ class StudyConfig(Controller):
                 self.study_config_file = None
         else:
                 self.study_config_file = self.global_config_file
-                    
+
         # Merge study configuration file with global configuration
         config.update(study_config)
-        
+
         return config
-        
+
     def get_configuration_dict(self):
         """ Returns a json compatible dictionary containing current
         configuration.
@@ -562,23 +598,22 @@ class StudyConfig(Controller):
         else:
             return None
 
-            
+
 class StudyConfigModule(object):
     @property
     def name(self):
-        """The name of a module that can be used in configuration to select 
+        """The name of a module that can be used in configuration to select
         modules to load.
         """
         return self.__class__.__name__
-    
-    # List of modules that must be initialized before this one. It can be 
+
+    # List of modules that must be initialized before this one. It can be
     # overrriden be derived module classes.
     dependencies = []
-    
+
     def __init__(self, study_config, configuration):
         self.study_config = study_config
-   
-   
+
     def initialize_module(self):
         """Method called to initialize selected study configuration modules
         on startup. This method does nothing but can be overriden by modules.
@@ -605,4 +640,3 @@ if __name__ == '__main__':
     study = StudyConfig(empty_config)
     toc = timeit.default_timer()
     print "Empty configuration done in  {0} s.".format(toc - tic)
-
