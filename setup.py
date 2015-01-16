02e0d023e98e9fe8e67bce14b98721ee5917a8fd
Antoine Grigis <antoine.grigis@cea.fr>
2015-01-16 13:28:51 +0000

    capsul: add options for gui and core deployment (todo split repo).
    git-svn-id: https://bioproj.extra.cea.fr/svn/capsul/trunk@387 89a606c0-f330-4fd0-a228-5aafc7605a1d

--- setup.py
+++ setup.py
@@ -9,17 +9,52 @@
 
 # System import
 from ez_setup import use_setuptools
+use_setuptools()
 import os
 from setuptools import find_packages, setup
+import argparse
+import sys
 
-# Get some information - parameters
-use_setuptools()
-release_info = {}
-execfile(os.path.join("capsul", "info.py"), release_info)
+# Select which package is created: core or gui
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("--gui", help="Create the gui package.",
+                    action="store_true")
+options, unknown = parser.parse_known_args()
+sys.argv = [sys.argv[0]] + unknown
+
+# Select appropriate modules
+modules = find_packages()
+core_modules = []
+gui_modules = ["capsul"]
+for module in modules:
+    if module.startswith("capsul.wip"):
+        continue
+    if module.startswith(("capsul.qt_apps", "capsul.qt_gui")):
+        gui_modules.append(module)
+    else:
+        core_modules.append(module)
+
+# Set selcted package options
+if options.gui:
+    import capsul
+    name_suffix = "gui"
+    modules = gui_modules
+    scripts = ["capsul/qt_apps/capsulview"]
+    pkgdata = {"capsul.qt_apps.resources": ["*.ui", "*.png", "*.qrc", "*.txt"]}
+    release_info = {}
+    execfile(os.path.join(os.path.dirname(capsul.__file__), "info.py"),
+             release_info)
+else:
+    name_suffix = "core"
+    modules = core_modules
+    scripts = []
+    pkgdata = {}
+    release_info = {}
+    execfile(os.path.join("capsul", "info.py"), release_info)
 
 # Build the setup
 setup(
-    name=release_info["NAME"],
+    name="{0}-{1}".format(release_info["NAME"], name_suffix),
     description=release_info["DESCRIPTION"],
     long_description=release_info["LONG_DESCRIPTION"],
     license=release_info["LICENSE"],
@@ -28,14 +63,10 @@ setup(
     author_email=release_info["AUTHOR_EMAIL"],
     version=release_info["VERSION"],
     url=release_info["URL"],
-    #package_dir = {"": "capsul"},
-    packages=find_packages(),
-    package_data={"capsul": ["qt_apps/resources/*.ui",
-                             "qt_apps/resources/*.png",
-                             "qt_apps/resources/*.qrc",
-                             "qt_apps/resources/*.txt"]},
+    packages=modules,
+    package_data=pkgdata,
     platforms=release_info["PLATFORMS"],
     extras_require=release_info["EXTRA_REQUIRES"],
     install_requires=release_info["REQUIRES"],
-    scripts=["capsul/qt_apps/capsulview", ]
+    scripts=scripts
 )
