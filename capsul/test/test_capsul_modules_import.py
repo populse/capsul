a008a2c5527919aea936b5482d4a275f68cc51a2
Denis Riviere <denis.riviere@cea.fr>
2015-01-20 15:38:43 +0000

    added a test to check that the main capsul submodules can be imported without problem. Fixes: #11780
    git-svn-id: https://bioproj.extra.cea.fr/svn/capsul/trunk@390 89a606c0-f330-4fd0-a228-5aafc7605a1d

---
+++ capsul/test/test_capsul_modules_import.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import unittest
+import sys
+import os
+
+
+class TestCapsulModulesImport(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_capsul_import(self):
+        import capsul
+
+    def test_capsul_studyconfig_import(self):
+        import capsul.study_config
+
+    def test_capsul_process_import(self):
+        import capsul.process
+
+    def test_capsul_pipeline_import(self):
+        import capsul.pipeline
+
+    def test_capsul_pipeline_pipeline_workflow_import(self):
+        import capsul.pipeline.pipeline_workflow
+
+    def test_capsul_utils_import(self):
+        import capsul.utils
+
+
+
+def test():
+    """ Function to execute unitest
+    """
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCapsulModulesImport)
+    runtime = unittest.TextTestRunner(verbosity=2).run(suite)
+    return runtime.wasSuccessful()
+
+
+if __name__ == "__main__":
+    print("RETURNCODE: ", test())
+
