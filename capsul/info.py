b9acfb72cfabcdccefc16ec8c99cc4001e6bbe00
denisri <denis.riviere.gm@gmail.com>
2015-05-21 01:06:24 +0200

    set project version 1.0.0, set optional dependencies

--- capsul/info.py
+++ capsul/info.py
@@ -8,9 +8,9 @@
 ##########################################################################
 
 # Capsul current version
-version_major = 0
+version_major = 1
 version_minor = 0
-version_micro = 1
+version_micro = 0
 version_extra = ""
 
 # The following variables are here for backward compatibility in order to
@@ -25,8 +25,9 @@ __version__ = "{0}.{1}.{2}{3}".format(
     version_major, version_minor, version_micro, version_extra)
 
 # Expected by setup.py: the status of the project
-CLASSIFIERS = ["Development Status :: 1 - Planning",
+CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
                "Environment :: Console",
+               "Environment :: X11 Applications :: Qt",
                "Operating System :: OS Independent",
                "Programming Language :: Python",
                "Topic :: Scientific/Engineering",
@@ -45,12 +46,16 @@ CAPSUL is a powerful tool to define and share processing pipelines.
 
 # Capsul dependencies
 SPHINX_MIN_VERSION = 1.0
-SOMA_WORKFLOW_MIN_VERSION = "2.7.0"
 SOMA_MIN_VERSION = "4.5.3"
 
 # dependencies
+SOMA_WORKFLOW_MIN_VERSION = "2.7.0"
+NIBABEL_MIN_VERSION = "1.0"
+NETWORKX_MIN_VERSION = "1.0"
 NUMPY_MIN_VERSION = "1.3"
+SCIPY_MIN_VERSION = "0.7"
 TRAITS_MIN_VERSION = "4.0"
+NIPYPE_VERSION = "0.10.0"
 XMLTODICT_MIN_VERSION = "0.9.0"
 
 # Main setup parameters
@@ -71,12 +76,24 @@ ISRELEASE = version_extra == ""
 VERSION = __version__
 PROVIDES = ["capsul"]
 REQUIRES = [
-    "numpy>={0}".format(NUMPY_MIN_VERSION),
     "traits>={0}".format(TRAITS_MIN_VERSION),
     "soma-base>={0}".format(SOMA_MIN_VERSION),
-    "soma-workflow>={0}".format(SOMA_WORKFLOW_MIN_VERSION),
     "xmltodict>={0}".format(XMLTODICT_MIN_VERSION)
 ]
 EXTRA_REQUIRES = {
-    "doc": ["sphinx>=1.0"]
+    "doc": [
+        "sphinx>=1.0",
+        "numpy>={0}".format(NUMPY_MIN_VERSION),
+    ],
+    "distributed": ["soma-workflow>={0}".format(SOMA_WORKFLOW_MIN_VERSION)],
+    "nipype": [
+        "numpy>={0}".format(NUMPY_MIN_VERSION),
+        "scipy>={0}".format(SCIPY_MIN_VERSION),
+        "nibabel>={0}".format(NIBABEL_MIN_VERSION),
+        "networkx>={0}".format(NETWORKX_MIN_VERSION)
+        "nipype=={0}".format(NIPYPE_VERSION),
+    ],
+
 }
+
+
