fa2d0315466aa6ac2318f860122861d2d06ce2dd
sapetnioc <sapetnioc@users.noreply.github.com>
2016-06-16 17:02:23 +0200

    Manage multiple schemas as well as fixed attributes values and editable attributes

--- capsul/attributes_schema.py
+++ capsul/attributes_schema.py
@@ -9,9 +9,13 @@
 from __future__ import print_function
 
 # System import
+import six
 from importlib import import_module
 from pkgutil import iter_modules
-import six
+from collections import OrderedDict
+
+from soma.controller import Controller
+from soma.functiontools import partial, SomaPartial
 
 class AttributesSchemaValidation(type):
     '''
@@ -41,16 +45,17 @@ class AttributesSchema(object):
     schema_name = None
 
     def __init__(self):
-        # Instanciate AttributeSet classes that are defined in schema
-        sets = dict((k,v()) for k, v in 
-                    six.iteritems(self.__class__.__dict__)
-                    if isinstance(v, type) and 
-                       issubclass(v, AttributeSet))
+        # Instanciate EditableAttributes classes that are defined in schema
+        sets = dict((k, getattr(self.__class__, k)()) for k in 
+                    dir(self.__class__)
+                    if isinstance(getattr(self.__class__, k), type) and 
+                       issubclass(getattr(self.__class__, k), EditableAttributes))
         self.attribute_sets = sets
 
 
-class AttributeSet(object):
-    pass
+class EditableAttributes(Controller):
+    def __str__(self):
+        return '<{0}({1})>'.format(self.__class__.__name__, None)
 
 
 def find_classes_in_module(module_name, parent_class):
@@ -75,7 +80,7 @@ def find_classes_in_module(module_name, parent_class):
                 yield j
 
 
-class AttributesSchemaManager(object):
+class AttributesSchemaFactory(object):
     '''
     This is the main entry point for finding and creating an attribute
     schema given its name and based to a serching path composed of Python
@@ -115,3 +120,71 @@ class AttributesSchemaManager(object):
                 self.attribute_schemas[schema_name] = attribute_schema
         return attribute_schema
 
+def set_attribute(object, name, value):
+    '''
+    
+    '''
+    setattr(object, name, value)
+
+class ProcessAttributes(Controller):
+    '''
+    This is the base class for managing attributes for a process.
+    '''
+    
+    def __init__(self, process, schema_dict):
+        super(ProcessAttributes, self).__init__()
+        self._process = process
+        self._schema_dict = schema_dict
+        self.editable_attributes = OrderedDict()
+        self.parameter_attributes = {}
+
+    
+    def set_parameter_attributes(self, parameter, schema, editable_attributes, fixed_attibute_values):
+        if parameter in self.parameter_attributes:
+            raise KeyError('Attributes already set for parameter %s' % parameter)
+        if isinstance(editable_attributes, six.string_types) or isinstance(editable_attributes, EditableAttributes):
+            editable_attributes = [editable_attributes]
+        parameter_editable_attributes = []
+        for ea in editable_attributes:
+            add_editable_attributes = False
+            if isinstance(ea, six.string_types):
+                key = ea
+                ea = self.editable_attributes.get(key)
+                if ea is None:
+                    ea = getattr(self._schema_dict[schema], key)()
+                    self.editable_attributes[key] = ea
+                    add_editable_attributes = True
+            elif isinstance(ea, EditableAttributes):
+                key = ea
+                if key not in self.editable_attributes:
+                    self.editable_attributes[key] = ea
+                    add_editable_attributes = True
+            else:
+                raise TypeError('Invalid value for editable attributes: {0}'.format(ea))
+            parameter_editable_attributes.append(ea)
+            if add_editable_attributes:
+                for name, trait in six.iteritems(ea.user_traits()):
+                    self.add_trait(name, trait)
+                    f = SomaPartial(set_attribute, ea)
+                    self.on_trait_change(f, name)
+        self.parameter_attributes[parameter] = (parameter_editable_attributes, fixed_attibute_values)
+
+    def get_parameters_attributes(self):
+        pa = {}
+        for parameter, trait in six.iteritems(self._process.user_traits()):
+            if trait.output:
+                attributes = {
+                    'generated_by_process': self._process.id,
+                    'generated_by_parameter': parameter}
+            else:
+                attributes = {}
+            editable_fixed = self.parameter_attributes.get(parameter, ([], {}))
+            editable_attributes, fixed_attibute_values = editable_fixed
+            for ea in editable_attributes:
+                for attribute in ea.user_traits():
+                    value = getattr(ea, attribute)
+                    attributes[attribute] = value
+            attributes.update(fixed_attibute_values)
+            if attributes:
+                pa[parameter] = attributes
+        return pa
