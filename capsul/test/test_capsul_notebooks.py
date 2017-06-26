22248fd094e888a8b20b7b7c69337ba511fbd032
Denis Rivière <denis.riviere.gm@gmail.com>
2017-06-26 15:49:30 +0200

    added test function for notebooks

---
+++ capsul/test/test_capsul_notebooks.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+
+import os
+import sys
+import glob
+from soma.test_utils import test_notebook as tnb
+import capsul
+
+# jupyter nbconvert import
+try:
+    import nbformat
+    from jupyter_core.command import main as main_jupyter
+except ImportError:
+    raise Warning('cannot import nbformat and/or jupyter_core.command: '
+                  'cannot test notebooks')
+
+
+def test():
+    os.environ["ALLOW_GUI"] = "FALSE"
+    root_dir = os.environ.get('CAPSUL_SOURCE_DIR', None)
+    if root_dir is None:
+        root_dir = os.path.dirname(capsul.__path__[0])
+    root_dir = os.path.abspath(root_dir)
+    nb_dir = os.path.join(root_dir, 'doc', 'source', '_static', 'tutorial')
+    notebooks = glob.glob(os.path.join(nb_dir, '*.ipynb'))
+    print('notebooks:', notebooks)
+    res = True
+    for notebook in notebooks:
+        res = tnb.test_notebook(notebook) or res
+    if res:
+        print('OK')
+    else:
+        print('FAILURE')
+    return res
+
+if __name__ == '__main__':
+    r = test()
+    if r:
+        res = 0
+    else:
+        res = 1
+    print("RETURNCODE: ", res)
+    sys.exit(res)
+
