e3f0df3403af2fe29f5856121647c4d39b3841eb
Denis Rivière <denis.riviere.gm@gmail.com>
2017-08-08 19:00:57 +0200

    added a test for optional output switch

---
+++ capsul/pipeline/test/test_optional_output_switch.py
@@ -0,0 +1,114 @@
+##########################################################################
+# CAPSUL - Copyright (C) CEA, 2013
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+from __future__ import print_function
+
+import unittest
+from traits.api import File, Float
+from capsul.api import Process
+from capsul.api import Pipeline
+from traits.api import Undefined
+
+
+class DummyProcess(Process):
+    """ Dummy Test Process
+    """
+    def __init__(self):
+        super(DummyProcess, self).__init__()
+
+        # inputs
+        self.add_trait("input_image", File(optional=False))
+
+        # outputs
+        self.add_trait("output_image", File(optional=True, output=True))
+
+    def __call__(self):
+        pass
+
+
+class MyPipelineWithOptOut(Pipeline):
+
+    """ Simple Pipeline to test the OptionalOutputSwitch Node
+    """
+    def pipeline_definition(self):
+
+        # Create processes
+        self.add_process("node1",
+            "capsul.pipeline.test.test_switch_optional_output.DummyProcess")
+        self.add_process("node2",
+            "capsul.pipeline.test.test_switch_optional_output.DummyProcess")
+        self.add_optional_output_switch("intermediate_out", "one")
+
+        # Links
+        self.add_link("node1.output_image->node2.input_image")
+        self.add_link("node1.output_image->"
+                      "intermediate_out.one_switch_intermediate_out")
+
+        # exports
+        self.export_parameter("node1", "input_image", "input_image1")
+        self.export_parameter("node2", "output_image", "output_image2")
+
+        self.node_position = {
+            'inputs': (-157.0, 67.8998),
+            'intermediate_out': (265.72604, 205.80563),
+            'node1': (46.40114, 102.8998),
+            'node2': (298.72604, 22.0),
+            'outputs': (540.34484, 109.8998)}
+
+class TestPipeline(unittest.TestCase):
+
+    def setUp(self):
+        self.pipeline = MyPipelineWithOptOut()
+
+    def test_way1(self):
+        self.pipeline.workflow_ordered_nodes()
+        self.assertEqual(self.pipeline.workflow_repr, "node1->node2")
+
+    def test_way2(self):
+        self.pipeline.workflow_ordered_nodes()
+        self.assertEqual(self.pipeline.workflow_repr, "node1->node2")
+        self.assertEqual(self.pipeline.nodes["intermediate_out"].switch,
+                         "_none")
+        self.pipeline.intermediate_out = '/tmp/a_file.txt'
+        self.assertEqual(self.pipeline.nodes["intermediate_out"].switch, "one")
+        self.assertEqual(self.pipeline.nodes["node1"].process.output_image,
+                         self.pipeline.intermediate_out)
+        self.pipeline.intermediate_out = Undefined
+        self.assertEqual(self.pipeline.nodes["intermediate_out"].switch,
+                         "_none")
+        self.assertEqual(self.pipeline.nodes["node1"].process.output_image,
+                         Undefined)
+
+
+def test():
+    """ Function to execute unitest
+    """
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPipeline)
+    runtime = unittest.TextTestRunner(verbosity=2).run(suite)
+    return runtime.wasSuccessful()
+
+
+if __name__ == "__main__":
+    print("RETURNCODE: ", test())
+
+    import sys
+    verbose = '-v' in sys.argv or '--verbose' in sys.argv
+
+    if verbose:
+        from PyQt4 import QtGui
+        from capsul.qt_gui.widgets import PipelineDevelopperView
+
+        app = QtGui.QApplication.instance()
+        if not app:
+            app = QtGui.QApplication(sys.argv)
+        pipeline = MyPipelineWithOptOut()
+        view1 = PipelineDevelopperView(pipeline, show_sub_pipelines=True,
+                                       allow_open_controller=True)
+        view1.show()
+        app.exec_()
+        del view1
