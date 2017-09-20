412c8e3f79db47f5ed9002ace818152390e4aa62
Denis Rivière <denis.riviere.gm@gmail.com>
2017-09-20 19:46:02 +0200

    increment version

--- capsul/info.py
+++ capsul/info.py
@@ -11,7 +11,7 @@ import sys
 # Capsul current version
 version_major = 2
 version_minor = 1
-version_micro = 0
+version_micro = 1
 version_extra = ""
 
 # The following variables are here for backward compatibility in order to
