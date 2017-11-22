cefa53d247b727655a71752d4721431c3e7311eb
sapetnioc <sapetnioc@users.noreply.github.com>
2017-11-22 16:56:40 +0100

    Removed obsolete files

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
-##########################################################################
-# CAPSUL - Copyright (C) CEA, 2013
-# Distributed under the terms of the CeCILL-B license, as published by
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-# for details.
-##########################################################################
-
-# System import
-from ez_setup import use_setuptools
-use_setuptools()
 import os
 from setuptools import find_packages, setup
-import argparse
-import sys
 
 # Select appropriate modules
 modules = find_packages()
