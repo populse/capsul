660fbc3a7f8d0142098f9fb54a739681c47ac6ad
Denis Rivière <denis.riviere.gm@gmail.com>
2018-10-18 12:53:01 +0200

    set version info to 2.2

--- capsul/info.py
+++ capsul/info.py
@@ -10,8 +10,8 @@ import sys
 
 # Capsul current version
 version_major = 2
-version_minor = 1
-version_micro = 6
+version_minor = 2
+version_micro = 0
 version_extra = ""
 
 # The following variables are here for backward compatibility in order to
