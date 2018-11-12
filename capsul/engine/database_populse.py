e662036c1ba9276ce51164a8d92789843e401d36
sapetnioc <sapetnioc@users.noreply.github.com>
2018-11-12 11:43:05 +0100

    Implementation and test of populse_db backend for CapsulEngine

--- capsul/engine/database_populse.py
+++ capsul/engine/database_populse.py
@@ -2,51 +2,103 @@ import os.path as osp
 import six
 import uuid
 
-from soma.serialization import JSONSerializable
+from capsul.engine.database import DatabaseEngine
 
 from populse_db.database import Database
 
-class PopulseDBEngine(JSONSerializable):
-    def __init__(self, database_engine, base_directory=None):
-        if base_directory:
-            self.base_directory = osp.normpath(osp.abspath(base_directory))
-        else:
-            self.base_directory = None
-        self. db = Database(database_engine)
+class PopulseDBEngine(DatabaseEngine):
+    def __init__(self, database_engine):
+        self.db = Database(database_engine)
         with self.db as dbs:
             if not dbs.get_collection('path_metadata'):
-                # Create the schema if it does not exists
+                # Create the schema if it does not exists                
+                dbs.add_collection('named_directory', 'name')
+                dbs.add_field('named_directory', 'path', 'string')
+                
+                dbs.add_collection('json_value', 'name')
+                dbs.add_field('json_value', 'value', 'json')
+                
                 dbs.add_collection('path_metadata', 'path')
                 dbs.add_field('path_metadata', 'named_directory', 'string', 
                               description='Reference to a base directory whose '
                               'path is stored in named_directory collection')
-                
-                dbs.add_collection('named_directory', 'name')
-                dbs.add_field('named_directory', 'path', 'string')
+        self.dbs = self.db.__enter__()
             
+    def commit(self):
+        self.dbs.save_modifications()
+    
+    def rollback(self):
+        self.dbs.unsave_modifications()
+    
     def set_named_directory(self, name, path):
-        with self.db as dbs:
+        if path:
+            path = osp.normpath(osp.abspath(path))
+        doc = self.dbs.get_document('named_directory', name)
+        if doc is None:
             if path:
-                path = osp.normpath(osp.abspath(path))
-            doc = dbs.get_document('named_directory', name)
-            if doc is None:
-                if path:
-                    doc = {'name': name,
-                           'path': path}
-                    dbs.add_document('named_directory', doc)
+                doc = {'name': name,
+                       'path': path}
+                self.dbs.add_document('named_directory', doc)
+        else:
+            if path:
+                self.dbs.set_value('named_directory', name, 'path', path)
             else:
-                if path:
-                    dbs.set_value('named_directory', name, 'path', path)
-                else:
-                    dbs.remove_documen('named_directory', name)
+                self.dbs.remove_document('named_directory', name)
     
     def named_directory(self, name):
-        with self.db as dbs:
-            return dbs.get_value('named_directory', name, 'path')
+        return self.dbs.get_value('named_directory', name, 'path')
     
     def named_directories(self):
-        with self.db as dbs:
-            return dbs.filter_documents('named_directory', 'all')
+        return self.dbs.filter_documents('named_directory', 'all')    
+        
+    def set_json_value(self, name, json_value):
+        doc = self.dbs.get_document('json_value', name)
+        json_dict = {'value': json_value}
+        if doc is None:
+            doc = {'name': name,
+                   'json_dict': json_dict
+                  }
+            self.dbs.add_document('json_value', doc)
+        else:
+            self.dbs.set_value('json_value', name, 'json_dict', json_dict)
+
+    def json_value(self, name):
+        doc = self.dbs.get_document('json_value', name)
+        if doc:
+            return doc['json_dict']['value']
+        return None
+    
+    
+    def set_path_metadata(self, path, metadata, named_directory=None):
+        metadata = self.check_metadata(path, metadata, named_directory)
+        path = metadata['path']
+        named_directory = metadata['named_directory']
+        
+        self.json_dict.setdefault('path_metadata', {})[(named_directory, path)] = doc
+        self.modified = True
+            
+
+    def path_metadata(self, path, named_directory=None):
+        named_directory, path = self.check_path(path, named_directory)
+        return self.json_dict.get('path_metadata', {}).get((named_directory, path))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
         
     def set_path_metadata(self, path, metadata):
--- capsul/engine/test/test_capsul_engine.py
+++ capsul/engine/test/test_capsul_engine.py
@@ -2,6 +2,13 @@ from __future__ import print_function
 
 import unittest
 import tempfile
+import os
+
+try:
+    import populse_db
+except ImportError:
+    populse_db = None
+
 from capsul.engine import engine
 
 class TestCapsulEngine(unittest.TestCase):
@@ -9,10 +16,31 @@ class TestCapsulEngine(unittest.TestCase):
         tmp = tempfile.mktemp(suffix='.json')
         ce = engine(tmp)
         ce.save()
-        ce2 = engine(tmp)
-        self.assertEqual(ce.execution_context.to_json(),
-                         ce2.execution_context.to_json())
+        try:
+            ce2 = engine(tmp)
+            self.assertEqual(ce.execution_context.to_json(),
+                             ce2.execution_context.to_json())
+            self.assertEqual(ce.database.named_directory('capsul_engine'),
+                             ce2.database.named_directory('capsul_engine'))
+        finally:
+            if os.path.exists(tmp):
+                os.remove(tmp)
         
+    def test_populse_db_engine(self):
+        if populse_db is None:
+            self.skipTest('populse_db is not installed')
+        tmp = tempfile.mktemp(suffix='.sqlite')
+        ce = engine(tmp)
+        ce.save()
+        try:
+            ce2 = engine(tmp)
+            self.assertEqual(ce.execution_context.to_json(),
+                             ce2.execution_context.to_json())
+            self.assertEqual(ce.database.named_directory('capsul_engine'),
+                             ce2.database.named_directory('capsul_engine'))
+        finally:
+            if os.path.exists(tmp):
+                os.remove(tmp)
 
 def test():
     """ Function to execute unitest.
