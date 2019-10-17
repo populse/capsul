b6dcf6f8ef670382e980bfd1f5cac48d09ad1c86
sapetnioc <sapetnioc@users.noreply.github.com>
2019-10-17 08:48:49 +0200

    Removed headers

--- capsul/qt_gui/__init__.py
+++ capsul/qt_gui/__init__.py
@@ -1,11 +1,3 @@
-##########################################################################
-# CAPSUL - Copyright (C) CEA, 2013
-# Distributed under the terms of the CeCILL-B license, as published by
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-# for details.
-##########################################################################
-
 '''
 Graphical representations and tools for processes and pipelines. The module contains mainly widgets, based on Qt, using either PyQt4, PyQt5 or PySide.
 '''
--- capsul/study_config/memory.py
+++ capsul/study_config/memory.py
@@ -1,13 +1,3 @@
-##########################################################################
-# CAPS - Copyright (C) CEA, 2013
-# Distributed under the terms of the CeCILL-B license, as published by
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-# for details.
-#
-# Based on the nipype memory code: http://nipy.sourceforge.net/nipype/
-##########################################################################
-
 '''
 Mamory caching. Probably mostly obsolete, this code is not much used now.
 
