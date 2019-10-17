c8658f80e5090489c366347461f5131393e2a5ca
sapetnioc <sapetnioc@users.noreply.github.com>
2019-10-17 08:48:49 +0200

    Removed headers

--- capsul/sphinxext/test/test_usercases_doc.py
+++ capsul/sphinxext/test/test_usercases_doc.py
@@ -1,11 +1,3 @@
-##########################################################################
-# Capsul - Copyright (C) CEA, 2014
-# Distributed under the terms of the CeCILL-B license, as published by
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-# for details.
-##########################################################################
-
 from __future__ import with_statement
 
 # System import
