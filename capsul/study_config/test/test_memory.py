fff65e41ef0517ac7771c6a177b2c4acd9603ce1
sapetnioc <sapetnioc@users.noreply.github.com>
2019-10-29 09:20:32 +0100

    Put back study_config tests

---
+++ capsul/study_config/test/test_memory.py
@@ -0,0 +1,135 @@
+from __future__ import print_function
+
+# System import
+import unittest
+import os
+import tempfile
+import shutil
+
+# Capsul import
+from capsul.api import Process
+from capsul.api import FileCopyProcess
+from capsul.api import get_process_instance
+from capsul.study_config.memory import Memory
+
+# Trait import
+from traits.api import Float, File, List, String
+
+
+class DummyProcess(Process):
+    """ Dummy.
+    """
+    f = Float(output=False, optional=False, desc="float")
+
+    def __init__(self):
+        super(DummyProcess, self).__init__()
+        self.add_trait("ff", Float(output=False, optional=True, desc="float"))
+        self.add_trait("res", Float(output=True, desc="float"))
+
+    def _run_process(self):
+        self.res = self.f * self.ff
+
+
+class DummyCopyProcess(FileCopyProcess):
+    """ Dummy file copy.
+    """
+    f = Float(output=False, optional=False, desc="a float")
+    i = File(output=False, optional=False, desc="a file")
+    l = List(File(), output=False, optional=False, desc="a list of file")
+    s = String(output=True, optional=False, desc="the output file copy map")
+
+    def _run_process(self):
+        self.s = repr(self.copied_inputs)
+
+
+class TestMemory(unittest.TestCase):
+    """ Execute a process using smart-caching functionalities.
+    """
+    def setUp(self):
+        self.workspace_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.workspace_dir)
+        if hasattr(self, 'cachedir'):
+            try:
+                shutil.rmtree(self.cachedir)
+            except:
+                pass
+
+    def test_proxy_process_with_cache(self):
+        """ Test the proxy process behaviours with cache.
+        """
+        # Create the memory object
+        self.cachedir = tempfile.mkdtemp()
+        self.mem = Memory(self.cachedir)
+    
+        # Call the test
+        self.proxy_process()
+
+        # Rm temporary folder
+        shutil.rmtree(self.cachedir)
+
+    def test_proxy_process_without_cache(self):
+        """ Test the proxy process behaviours without cache.
+        """
+        # Create the memory object
+        self.cachedir = None
+        self.mem = Memory(self.cachedir)
+    
+        # Call the test
+        self.proxy_process()
+
+    def test_proxy_process_copy(self):
+        """ Test memory with copy.
+        """
+        # Create the memory object
+        self.cachedir = None
+        self.mem = Memory(self.cachedir)
+    
+        # Call the test
+        self.proxy_process_copy()
+
+    def proxy_process(self):
+        """ Test the proxy process behaviours.
+        """
+        # Create a process instance
+        process = DummyProcess()
+
+        # Create a proxy process
+        proxy_process = self.mem.cache(process, verbose=1)
+
+        # Test the cache mechanism
+        for param in [(1., 2.3), (2., 2.), (1., 2.3)]:
+            proxy_process(f=param[0], ff=param[1])
+            self.assertEqual(proxy_process.res, param[0] * param[1])
+
+    def proxy_process_copy(self):
+        """ Test memory with copy.
+        """
+        # Create a process instance
+        process = DummyCopyProcess()
+        process.destination = self.workspace_dir
+
+        # Create a proxy process
+        proxy_process = self.mem.cache(process, verbose=1)
+
+        # Test the cache mechanism
+        proxy_process(f=2.5, i=__file__, l=[__file__])
+        copied_file = os.path.join(self.workspace_dir,
+                                   os.path.basename(__file__))
+        print('proxy_process:', type(proxy_process))
+        print('machin:', "{{'i': {0}, 'l': [{0}], 'f': 2.5}}".format(repr(copied_file)))
+        self.assertEqual(
+            eval(proxy_process.s),
+            {'i': copied_file, 'l': [copied_file], 'f': 2.5})
+
+def test():
+    """ Function to execute unitest.
+    """
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMemory)
+    runtime = unittest.TextTestRunner(verbosity=2).run(suite)
+    return runtime.wasSuccessful()
+
+
+if __name__ == "__main__":
+    print("RETURNCODE: ", test())
---
+++ capsul/study_config/test/test_run_process.py
@@ -0,0 +1,109 @@
+# System import
+import unittest
+import tempfile
+import shutil
+
+# Capsul import
+from capsul.api import Process, get_process_instance
+from capsul.study_config.run import run_process
+
+# Trait import
+from traits.api import Float, Directory, File
+
+
+class DummyProcess(Process):
+    """ A dummy process.
+    """
+    f1 = Float(output=False, optional=False, desc="a float")
+    f2 = Float(output=False, optional=False, desc="a float")
+    output_directory = Directory(output=False, optional=False,
+                                 exists=False, desc="a directory")
+    res = Float(output=True, desc="a float")
+
+    def _run_process(self):
+        self.res = self.f1 * self.f2
+
+class DummyProcess2(Process):
+    """ A dummy process.
+    """
+    f1 = Float(output=False, optional=False, desc="a float")
+    f2 = Float(output=False, optional=False, desc="a float")
+    output_file = File(output=True, optional=False, input_filename=True,
+                       desc="an output file")
+
+    def _run_process(self):
+        open(self.output_file, 'w').write(str(self.f1 * self.f2))
+
+
+class TestRunProcess(unittest.TestCase):
+    """ Execute a process.
+    """
+    def test_execution_with_cache(self):
+        """ Execute a process with cache.
+        """
+        # Create a study configuration
+        self.output_dir = tempfile.mkdtemp()
+        try:
+            self.cachedir = self.output_dir
+
+            # Call the test
+            self.execution_dummy()
+        finally:
+            # Rm temporary folder
+            shutil.rmtree(self.output_dir)
+
+    def test_execution_without_cache(self):
+        """ Execute a process without cache.
+        """
+        # Create a study configuration
+        self.output_dir = tempfile.mkdtemp()
+        try:
+            self.cachedir = None
+
+            # Call the test
+            self.execution_dummy()
+        finally:
+            # Rm temporary folder
+            shutil.rmtree(self.output_dir)
+
+    def execution_dummy(self):
+        """ Test to execute DummyProcess.
+        """
+        # Create a process instance
+        process = get_process_instance(DummyProcess, output_directory=self.output_dir)
+
+        # Test the cache mechanism
+        for param in [(1., 2.3), (2., 2.), (1., 2.3)]:
+            run_process(self.output_dir, process, cachedir=self.cachedir,
+                        generate_logging=False, verbose=1,
+                        f1=param[0], f2=param[1])
+            self.assertEqual(process.res, param[0] * param[1])
+            self.assertEqual(process.output_directory, self.output_dir)
+
+    def test_missing_parameters(self):
+        """ Test if missing mandatory parameters raise the appropriate error
+        """
+        # Create a process instance
+        process = get_process_instance(DummyProcess2)
+
+        self.assertRaises(ValueError, process, f1=1., f2=2.3)
+
+
+def test():
+    """ Function to execute unitest.
+    """
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRunProcess)
+    runtime = unittest.TextTestRunner(verbosity=2).run(suite)
+    return runtime.wasSuccessful()
+
+    dirpath = tempfile.mkdtemp()
+    suite = unittest.TestSuite()
+    suite.addTest(TestRunProcess("test_execution_1", dirpath, dirpath))
+    suite.addTest(TestRunProcess("test_execution_1", dirpath, None))
+    runtime = unittest.TextTestRunner(verbosity=2).run(suite)
+    shutil.rmtree(dirpath)
+    return runtime.wasSuccessful()
+
+
+if __name__ == "__main__":
+    print("RETURNCODE: ", test())
