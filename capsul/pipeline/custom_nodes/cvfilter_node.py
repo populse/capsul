ce285b7fbf62af89b5e98c91453d406689fdf363
denisri <denis.riviere.gm@gmail.com>
2020-01-10 17:06:51 +0100

    rewriting crossvalidation node

--- capsul/pipeline/custom_nodes/cvfilter_node.py
+++ capsul/pipeline/custom_nodes/cvfilter_node.py
@@ -25,10 +25,10 @@ class CVFilterNode(Node):
     def __init__(self, pipeline, name, is_output=True, input_type=None):
         in_traitsl = ['inputs', 'fold', 'nfolds']
         if is_output:
-            out_traitsl = ['learn_list', 'test_list']
+            out_traitsl = ['train', 'test']
         else:
             out_traitsl = []
-            in_traitsl += ['learn_list', 'test_list']
+            in_traitsl += ['train', 'test']
         in_traits = []
         out_traits = []
         for tr in in_traitsl:
@@ -45,8 +45,8 @@ class CVFilterNode(Node):
         self.add_trait('inputs', traits.List(ptype, output=False))
         self.add_trait('fold', traits.Int())
         self.add_trait('nfolds', traits.Int(10))
-        self.add_trait('learn_list', traits.List(ptype, output=is_output))
-        self.add_trait('test_list', traits.List(ptype, output=is_output))
+        self.add_trait('train', traits.List(ptype, output=is_output))
+        self.add_trait('test', traits.List(ptype, output=is_output))
 
         self.set_callbacks()
 
@@ -91,4 +91,98 @@ class CVFilterNode(Node):
         node = CVFilterNode(pipeline, name, conf_controller.is_output,
                             input_type=t)
         return node
+
+    def is_job(self):
+        return self.trait('train').output
+
+    def params_to_command(self):
+        return ['custom_job']
+
+    def build_job(self):
+        from soma_workflow.pipeline.custom_jobs import CrossValidationJob
+        job = CrossValidationJob()
+        return job
+
+
+class CrossValidationNode(Node):
+    '''
+    This "inert" node filters a list to separate it into (typically) learn and
+    test sublists.
+
+    The "outputs" are "train" and "test" output traits.
+    '''
+
+    def __init__(self, pipeline, name, input_type=None):
+        in_traitsl = ['inputs', 'fold', 'nfolds']
+        out_traitsl = ['train', 'test']
+        in_traits = []
+        out_traits = []
+        for tr in in_traitsl:
+            in_traits.append({'name': tr, 'optional': True})
+        for tr in out_traitsl:
+            out_traits.append({'name': tr, 'optional': True})
+        super(CrossValidationNode, self).__init__(pipeline, name, in_traits,
+                                                  out_traits)
+        if input_type:
+            ptype = input_type
+        else:
+            ptype = traits.Any(traits.Undefined)
+
+        self.add_trait('inputs', traits.List(ptype, output=False))
+        self.add_trait('fold', traits.Int())
+        self.add_trait('nfolds', traits.Int(10))
+        self.add_trait('train', traits.List(ptype, output=is_output))
+        self.add_trait('test', traits.List(ptype, output=is_output))
+
+        self.set_callbacks()
+
+    def set_callbacks(self, update_callback=None):
+        inputs = ['inputs', 'fold', 'nfolds']
+        if update_callback is None:
+            update_callback = self.filter_callback
+        for name in inputs:
+            self.on_trait_change(update_callback, name)
+
+    def filter_callback(self):
+        n = len(self.inputs) // self.nfolds
+        ninc = len(self.inputs) % self.nfolds
+        begin = self.fold * n + min((ninc, self.fold))
+        end = min((self.fold + 1) * n + min((ninc, self.fold + 1)),
+                  len(self.inputs))
+        self.train = self.inputs[:begin] + self.inputs[end:]
+        self.test =  self.inputs[begin:end]
+
+    def configured_controller(self):
+        c = self.configure_controller()
+        c.param_type = self.trait('inputs').inner_traits[0].trait_type.__class__.__name__
+        return c
+
+    @classmethod
+    def configure_controller(cls):
+        c = Controller()
+        c.add_trait('param_type', traits.Str('Str'))
+        return c
+
+    @classmethod
+    def build_node(cls, pipeline, name, conf_controller):
+        t = None
+        if conf_controller.param_type == 'Str':
+            t = traits.Str(traits.Undefined)
+        elif conf_controller.param_type == 'File':
+            t = traits.File(traits.Undefined)
+        elif conf_controller.param_type not in (None, traits.Undefined):
+            t = getattr(traits, conf_controller.param_type)()
+        node = CrossValidationNode(pipeline, name, input_type=t)
+        return node
+
+    def is_job(self):
+        return True
+
+    def params_to_command(self):
+        return ['custom_job']
+
+    def build_job(self):
+        from soma_workflow.pipeline.custom_jobs import CrossValidationJob
+        job = CrossValidationJob()
+        return job
 
