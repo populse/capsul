b947801901d1fc8145cdc1cb704b41ad15360354
Denis Rivière <denis.riviere.gm@gmail.com>
2022-04-01 18:33:10 +0200

    moved engine config GUI factory to capsul (#145)
    To edit a capsul config, you need to import the module
    capsul.qt_gui.widghets.config_gui, then instantiate a soma ControllerWidget
    on the config object. The import will update the factories to register
    the engine config specialization.

---
+++ capsul/qt_gui/widgets/config_gui.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from soma.qt_gui.controller import (
+    ControllerWidget, WidgetFactory, OpenKeyControllerWidgetFactory,
+    ControllerWidgetFactory, ControllerSubwidget)
+from soma.qt_gui.collapsable import CollapsableWidget
+from soma.controller import undefined
+from soma.qt_gui.qt_backend import Qt
+from functools import partial
+from capsul.config.configuration import ApplicationConfiguration
+
+
+class EngineConfigurationWidgetFactory(ControllerWidgetFactory):
+    def create_widgets(self):
+        controller = self.parent_interaction.get_value()
+        if controller is undefined:
+            controller = self.parent_interaction.field.type()
+        self.inner_widget = ControllerSubwidget(
+            controller, readonly=self.readonly,
+            depth=self.controller_widget.depth + 1)
+        label = self.parent_interaction.get_label()
+        if not self.readonly:
+            buttons = ['+']
+        else:
+            buttons = []
+        self.widget = CollapsableWidget(
+            self.inner_widget, label=label,
+            expanded=(self.parent_interaction.depth==0),
+            buttons_label=buttons,
+            parent=self.controller_widget)
+        self.inner_widget.setContentsMargins(
+            self.widget.toggle_button.sizeHint().height(),0,0,0)
+
+        if not self.readonly:
+            button = self.widget.buttons[0]
+            button.setPopupMode(button.InstantPopup)
+            menu = Qt.QMenu()
+            custom = menu.addAction('add custom module')
+            custom.triggered.connect(self.add_custom_module)
+            available_modules = [
+                x.rsplit('.', 1)[-1]
+                for x in ApplicationConfiguration.available_modules()]
+            for module in available_modules:
+                action = menu.addAction(module)
+                action.triggered.connect(partial(self.add_module, module))
+            button.setMenu(menu)
+
+        self.controller_widget.add_widget_row(
+            self.widget, label_index=0,
+            field_name=self.parent_interaction.field.name)
+        self.parent_interaction.on_change_add(self.update_gui)
+
+    def add_module(self, module):
+        controller = self.parent_interaction.get_value()
+        controller.add_module(module)
+
+    def add_custom_module(self):
+        module = self.inner_widget.ask_new_key_name()
+        if module:
+            controller = self.parent_interaction.get_value()
+            controller.add_module(module)
+
+
+WidgetFactory.widget_factory_types.update({
+    'ConfigurationLayer': OpenKeyControllerWidgetFactory,
+    'EngineConfiguration': EngineConfigurationWidgetFactory,
+    'Controller[capsul.config.configuration.EngineConfiguration]':
+        EngineConfigurationWidgetFactory,
+})
