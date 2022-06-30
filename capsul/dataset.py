4bc8aa888d4b0a34d3eaebd02ca33b369fcdde03
sapetnioc <sapetnioc@users.noreply.github.com>
2022-06-30 15:50:59 +0200

    Allow to use string for process in ProcessSchema definition

--- capsul/dataset.py
+++ capsul/dataset.py
@@ -388,7 +388,11 @@ class BidsToBrainVISA(SchemaMapping):
 
 class ProcessSchema:
     def __init_subclass__(cls, schema, process) -> None:
+        from .application import get_node_class
+
         super().__init_subclass__()
+        if isinstance(process, str):
+            process = get_node_class(process)[1]
         if 'metadata_schemas' not in process.__dict__:
             process.metadata_schemas = {}
         schemas = process.metadata_schemas
