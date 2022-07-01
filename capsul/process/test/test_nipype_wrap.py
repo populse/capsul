cff80e2dd7f718d205e89a9b28573d8f0a22513c
Denis Rivière <denis.riviere.gm@gmail.com>
2022-07-01 12:21:18 +0200

    added a conditional nipype/SPM execution test

--- capsul/process/test/test_nipype_wrap.py
+++ capsul/process/test/test_nipype_wrap.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 # System import
 from __future__ import absolute_import
+
 import os
+import os.path as osp
 import unittest
+import glob
+import shutil
+import tempfile
 
 # Capsul import
 from capsul.api import executable
 from capsul.api import NipypeProcess
+from capsul.api import Capsul
+from capsul.config.configuration import ApplicationConfiguration
 
 try:
     import nipype
@@ -29,6 +36,64 @@ class TestNipypeWrap(unittest.TestCase):
             # default is nifti
             self.output_extension = '.nii'
 
+    @staticmethod
+    def init_spm_config():
+        if nipype is None:
+            return False
+
+        spm_search_dirs = ['/host/usr/local/spm12-standalone',
+                           '/usr/local/spm12-standalone',
+                           '/i2bm/local/spm12-standalone']
+        mcr_search_dirs = ['/host/usr/local/Matlab/mcr/v97',
+                           '/usr/local/Matlab/mcr/v97',
+                           '/i2bm/local/Matlab/mcr/v97']
+
+        spm_dir = None
+        for spm_dir in spm_search_dirs:
+            if osp.isdir(spm_dir):
+                break
+        if not osp.isdir(spm_dir):
+            return False
+
+        mcr_dir = None
+        mcr_dirs = glob.glob(osp.join(spm_dir, 'mcr', 'v*'))
+        if len(mcr_dirs) == 1:
+            mcr_dir = mcr_dirs[0]
+        else:
+            for mcr_dir in mcr_search_dirs:
+                if osp.isdir(mcr_dir):
+                    break
+        if not osp.isdir(mcr_dir):
+            return False
+
+        c = Capsul()
+
+        config = ApplicationConfiguration('capsul_test_nipype_spm')
+        user_conf_dict = {
+            'local': {
+                'spm': {
+                    'spm12_standalone': {
+                        'directory': spm_dir,
+                        'standalone': True,
+                        'version': '12'},
+                    },
+                'nipype': {
+                    'nipype': {},
+                },
+                'matlab': {
+                    'matlab_mcr': {
+                        'mcr_directory': mcr_dir,
+                    },
+                },
+            }
+        }
+        config.user = user_conf_dict
+        config.merge_configs()
+
+        c.config = config.merged_config
+        # print('local config:', c.config.asdict())
+
+        return True
 
     #@unittest.skip('reimplementation expected for capsul v3')
     @unittest.skipIf(nipype is None, 'nipype is not installed')
@@ -41,7 +106,7 @@ class TestNipypeWrap(unittest.TestCase):
         self.assertTrue(isinstance(nipype_process, NipypeProcess))
         self.assertTrue(isinstance(nipype_process._nipype_interface, BET))
 
-    @unittest.skip('reimplementation expected for capsul v3')
+    #@unittest.skip('reimplementation expected for capsul v3')
     @unittest.skipIf(nipype is None, 'nipype is not installed')
     def test_nipype_monkey_patching(self):
         """ Method to test the monkey patching used to work in user
@@ -53,6 +118,58 @@ class TestNipypeWrap(unittest.TestCase):
             nipype_process._nipype_interface._list_outputs()["out_file"],
             os.path.join(os.getcwd(),
                          "test_nipype_wrap_brain%s" % self.output_extension))
+
+    @unittest.skipIf(not init_spm_config(),
+                     'SPM is not configured to run this test')
+    def test_nipype_spm_exec(self):
+
+        c = Capsul()
+
+        template_dirs = ['spm12_mcr/spm12/spm12',
+                         'spm12_mcr/spm12',
+                         'spm12']
+        for template_dir_s in template_dirs:
+            template_dir = osp.join(
+                c.config.local.spm.spm12_standalone.directory, template_dir_s)
+            if osp.isdir(template_dir):
+                break
+        self.assertTrue(
+            osp.isdir(template_dir),
+            'SPM template dir is not found. Please check SPM installation')
+
+        t1_template = osp.join(template_dir, 'toolbox/OldNorm/T1.nii')
+
+        tmp_dir = tempfile.mkdtemp(prefix='capsul_nipype_wrap_test')
+
+        try:
+
+            p = c.executable('nipype.interfaces.spm.preprocess.Smooth')
+
+            p.in_files = [t1_template]
+            p.fwhm = 10.
+            p.output_directory = tmp_dir
+
+            with c.engine() as e:
+                e.run(p)
+
+            print('output values:')
+            print(p.asdict())
+
+            smoothed = p.smoothed_files
+            self.assertEqual(len(smoothed), 1)
+            smoothed = smoothed[0]
+            self.assertEqual(osp.dirname(smoothed), tmp_dir)
+            self.assertEqual(osp.basename(smoothed),
+                             f's{osp.basename(t1_template)}')
+            self.assertTrue(osp.exists(smoothed))
+
+        finally:
+            shutil.rmtree(tmp_dir)
+
+
+def tearDownModule():
+    Capsul.delete_singleton()
+
 
 def test():
     """ Function to execute unitest
