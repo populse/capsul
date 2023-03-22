6ec9877e06403d1af779e7fb65df15286e6db3c6
sapetnioc <sapetnioc@users.noreply.github.com>
2023-03-22 16:43:35 +0100

    added pytest -x and -k parameters to python -m capsul.test command

--- capsul/test/__main__.py
+++ capsul/test/__main__.py
@@ -13,12 +13,40 @@ parser.add_argument('--html',
                     metavar='DIRECTORY',
                     help='Create tests and coverage HTML reports in given '
                     'directory. Entrypoints are test.html and index.html')
+parser.add_argument('-x', '--exitfirst', 
+                    action='store_true',
+                    help='Exit instantly on first error or failed test')
+parser.add_argument('-k',
+                    metavar='EXPRESSION',
+                    dest='keyword',
+                    default=None,
+                    action='store',
+                    help='Only run tests which match the given substring '
+                         'expression. An expression is a Python evaluatable '
+                         'expression where all names are substring-matched '
+                         'against test names and their parent classes. '
+                         "Example: -k 'test_method or test_other' matches "
+                         'all test functions and classes whose name contains '
+                         "'test_method' or 'test_other', while -k 'not "
+                         "test_method'vmatches those that don't contain "
+                         "'test_method' in their names. -k 'not test_method "
+                         "and not test_other' will eliminate the matches. "
+                         'Additionally keywords are matched to classes and '
+                        'functions containing extra names in their '
+                        "'extra_keyword_matches' set, as well as functions "
+                        'which have names assigned directly to them. The '
+                        'matching is case-insensitive.')
+
 args = parser.parse_args()
 if args.html:
     Path(args.html).mkdir(exist_ok=True)
     pytest_command = [sys.executable, '-m', 'coverage', 'run', '--source=.',
                     '-m', 'pytest',
                     '--html={}/tests.html'.format(args.html)]
+    if args.exitfirst:
+        pytest_command.append('-x')
+    if args.keyword:
+        pytest_command.extend(['-k', args.keyword])
     coverage_command = [sys.executable, '-m', 'coverage', 'html', '-d', args.html]
     print(' '.join("'{}'".format(i) for i in pytest_command))
     subprocess.check_call(pytest_command)
