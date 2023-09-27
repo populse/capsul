eeffa26ede8d1c0af4e06c98d24649afc32d92b4
sapetnioc <sapetnioc@users.noreply.github.com>
2023-09-27 17:45:03 +0200

    #271 fixed web GUI according to changes in soma-base

--- capsul/ui/__init__.py
+++ capsul/ui/__init__.py
@@ -1,23 +1,15 @@
 # -*- coding: utf-8 -*-
-
-from capsul.api import Capsul
-from capsul.web import WebRoutes, WebBackend
-
-from soma.qt_gui.qt_backend import Qt
+from soma.web import WebRoutes, WebBackend
 
 
 class CapsulRoutes(WebRoutes):
-    _templates = {
-        'qt_backend.js',
-        'html_backend.js'
-    }
     def dashboard(self):
         return self._result('dashboard.html')
 
 
 
     def engine(self, engine_label):
-        engine = self.handler.jinja_kwargs['capsul'].engine(engine_label)
+        engine = self._handler['capsul'].engine(engine_label)
         if engine:
             return self._result('engine.html', engine=engine)
 
@@ -29,28 +21,22 @@ class CapsulRoutes(WebRoutes):
 
 
 class CapsulBackend(WebBackend):
-
-    def capsul(self):
-        if not hasattr(self, '_capsul'):
-            self._capsul = Capsul()
-        return self._capsul
-
     def engines(self) -> list:
-        return [engine.engine_status() for engine in self.capsul().engines()]
+        return [engine.engine_status() for engine in self._handler['capsul'].engines()]
 
 
     def engine_status(self, engine_label: str) -> dict:
         try:
-            engine = self.capsul().engine(engine_label)
+            engine = self._handler['capsul'].engine(engine_label)
         except ValueError:
             return {}
         return engine.engine_status()
 
 
     def executions_summary(self, engine_label: str) -> list:
-        return self.capsul().engine(engine_label).executions_summary()
+        return self._handler['capsul'].engine(engine_label).executions_summary()
 
 
     def execution_report(self, engine_label: str, execution_id: str) -> dict:
-        with self.capsul().engine(engine_label) as engine:
+        with self._handler['capsul'].engine(engine_label) as engine:
             return engine.database.execution_report_json(engine.engine_id, execution_id)
