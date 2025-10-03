6e2974efae7016703710fd68107dc7075871f6bb
sapetnioc <sapetnioc@users.noreply.github.com>
2025-10-03 17:25:59 +0200

    Set package capsul from version 2.6.7 to version 2.6.8

--- capsul/info.py
+++ capsul/info.py
@@ -9,7 +9,7 @@ import sys
 # Capsul current version
 version_major = 2
 version_minor = 6
-version_micro = 7
+version_micro = 8
 version_extra = ""
 
 # The following variables are here for backward compatibility in order to
