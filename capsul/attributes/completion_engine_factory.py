4f325ab2e063d0cd4687c3afb9bf0c1c7763db9b
Eric Condamine <37933899+servoz@users.noreply.github.com>
2025-10-29 07:49:32 +0100

    Fix comparison operator in completion engine factory

--- capsul/attributes/completion_engine_factory.py
+++ capsul/attributes/completion_engine_factory.py
@@ -40,7 +40,7 @@ class BuiltinProcessCompletionEngineFactory(ProcessCompletionEngineFactory):
         if hasattr(process, 'completion_engine'):
             ce = process.completion_engine
             # just in case the process has been changed in the CE
-            if ce and ce.process is not process:
+            if ce and ce.process != process:
                 ce.process = weak_proxy(process, ce._clear_node)
             return ce
 
